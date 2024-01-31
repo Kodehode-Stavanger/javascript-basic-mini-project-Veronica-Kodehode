706852af88c9480295b65c776a953770c61b3fd8
Veronica Kodehode <Veronicas.kodehode@gmail.com>
2024-01-31 14:36:57 +0100

    overlapping cards

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const twelve = document.querySelector("#play")
+const twelve = document.querySelector("#play");
 twelve.addEventListener("click", playCards);
-
 
 const cards = [
   "purple",
@@ -14,39 +13,47 @@ const cards = [
   "brown",
   "grey",
   "white",
-  "lightblue"
+  "lightblue",
 ];
 
 // duplicate array
 
 const duplicated = [...cards, ...cards];
 
-
 // function to run when button clicked
 
-function playCards () {
+function playCards() {
+  // while loop to clear cards before generating new cards
 
-// while loop to clear cards before generating new cards
-
-while (playarea.firstChild) {
+  while (playarea.firstChild) {
     playarea.removeChild(playarea.firstChild);
+  }
+
+  // for loop to randomize duplicated array
+
+  for (let i = duplicated.length - 1; i > 0; i--) {
+    let a = Math.floor(Math.random() * (i + 1));
+    let b = duplicated[i];
+    duplicated[i] = duplicated[a];
+    duplicated[a] = b;
+  }
+
+  // loop to generate cards, styled
+
+  for (let i = 0; i < duplicated.length; i++) {
+    let cardContainer = document.createElement("div");
+    cardContainer.className = "card-container";
+    playarea.appendChild(cardContainer);
+
+    let card = document.createElement("div");
+    card.className = "card";
+    card.style.backgroundColor = duplicated[i];
+    cardContainer.appendChild(card);
+
+    let back = document.createElement("div");
+    back.className = "back";
+    cardContainer.appendChild(back);
+  }
 }
 
-// for loop to randomize duplicated array
-
-    for (let i = duplicated.length -1; i > 0; i--) {
-        let a = Math.floor(Math.random() * (i+1));
-        let b = duplicated[i];
-        duplicated[i] = duplicated[a];
-        duplicated[a] = b;
-    }
-
-// loop to generate cards, styled
-
-    for (let i = 0; i < duplicated.length; i++) {   
-        let card = document.createElement("div");
-        card.className = "card";
-        card.style.backgroundColor = duplicated[i];
-        playarea.appendChild(card);
-    }
-}
+// conditionals
